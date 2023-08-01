1749d6c7f9a2734b0f0d2c88f383850ad21a0ac8
Frontend Bimakavach <133082234+bkFrontend123@users.noreply.github.com>
2023-08-01 20:37:33 +0530

    update

--- component/Product/BestBacked.js
+++ component/Product/BestBacked.js
@@ -26,7 +26,6 @@ export default function BestBacked(props) {
                     <Row className={`text-center`}>
                         <Col md={8} className={`mx-auto`}>
                             <div className='commnHeading'>
-                                <div className='polyBox border-primary bg-blueLight text-primary title12 text-uppercase font-weight-600 mb-3'>Our Investors</div>
                                 <div className={`title-36 font-secondary mb-4 text-primary font-weight-700 ${props.titleClass}`}>Backed by <span className="text-yellow fst-italic">The Best</span></div>
                             </div>
                         </Col>
