fb587f76a55b57476ad79d8daddef3e22d6a33ac
Frontend Bimakavach <133082234+bkFrontend123@users.noreply.github.com>
2023-08-09 14:03:25 +0530

    Update

--- component/BIDashboard/DashboardSidebar/index.js
+++ component/BIDashboard/DashboardSidebar/index.js
@@ -66,7 +66,7 @@ export default function DashboardSidebar(props) {
        <SidebarLinkItem
           title="Logout"
           icon={<LogoutIcon/>}
-          href="/"
+          href="/dashboard/login"
         />
       </div>
     </>
