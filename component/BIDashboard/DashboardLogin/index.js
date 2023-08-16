7cfd04143e0774f6028c1d480d1f5d2bda136cbf
Frontend Bimakavach <133082234+bkFrontend123@users.noreply.github.com>
2023-08-16 18:46:35 +0530

    Update

--- component/BIDashboard/DashboardLogin/index.js
+++ component/BIDashboard/DashboardLogin/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from 'next/image'
 
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -17,9 +18,74 @@ export default function DashboardLogin(props) {
               <Row className="align-items-center justify-content-between">
                 <Col lg={6}>
                   <div className={`${loginLayoutStyle.loginLeft}`}>
-                    <DashboardLoginHeading
-                      title="Your business needs Insurance too"
-                    />
+                    <div className={`${loginLayoutStyle.loginHeroMediaOuter}`}>
+                      <div className={`${loginLayoutStyle.loginHeroShield}`}>
+                        <img src="/icons/loginShield.svg" alt="Shield" />
+                      </div>
+                      <div className={`${loginLayoutStyle.loginHeroImg}`}>
+                        <img src="/loginImg.png" alt="Dilip Joshi" />
+                      </div>
+                      <div className={`${loginLayoutStyle.loginHeroTextDiv}`}>
+                        <div className={`${loginLayoutStyle.loginHeroText}`}>
+                          <p>
+                            <i className='d-none d-md-block'>
+                                <Image
+                                    src="/icons/getBusinessIcon.svg"
+                                    alt={[<>Get Business Insurance in Minutes</>]}
+                                    width={30}
+                                    height={36}
+                                />
+                            </i>
+                            <i className='d-md-none'>
+                                <Image
+                                    src="/icons/getBusinessIcon.svg"
+                                    alt={[<>Get Business Insurance in Minutes</>]}
+                                    width={20}
+                                    height={26}
+                                />
+                            </i> Get Business Insurance in Minutes
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className={`${loginLayoutStyle.loginBottom}`}>
+                      <div className={`${loginLayoutStyle.loginBottomCol}`}>
+                        <i>
+                          <Image
+                            src="/icons/simplifiedIcon.svg"
+                            width={33}
+                            height={20}
+                            alt="Simplified Business Icon"
+                            loading="lazy"
+                          />
+                        </i>
+                        <p>Simplified Business Insurance</p>
+                      </div>
+                      <div className={`${loginLayoutStyle.loginBottomCol}`}>
+                        <i>
+                          <Image
+                            src="/icons/supportHomeIcon.svg"
+                            width={34}
+                            height={34}
+                            alt="Support Icon"
+                            loading="lazy"
+                          />
+                        </i>
+                        <p>Easy 24*7 Claims Support</p>
+                      </div>
+                      <div className={`${loginLayoutStyle.loginBottomCol}`}>
+                        <i>
+                          <Image
+                            src="/icons/bestPriceIcon.svg"
+                            width={32}
+                            height={32}
+                            alt="Best Price Icon"
+                            loading="lazy"
+                          />
+                        </i>
+                        <p>20+ Insurers with Best Prices</p>
+                      </div>
+                    </div>
                   </div>
                 </Col>
                 <Col lg={6}>
